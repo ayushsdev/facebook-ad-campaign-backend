2533228c46b0426a8c24808554f7ceaabcf6ca28
ayushsdev <asharmadev314@gmail.com>
2024-09-22 23:10:58 -0500

    Fixed aws security issues

--- api/utils.py
+++ api/utils.py
@@ -2,13 +2,14 @@ import boto3
 import pickle
 from io import BytesIO
 import pandas as pd
+import os
 
 def download_pickle_from_s3(file_name):
     # Hardcoded AWS credentials and S3 bucket information (for development purposes)
-    aws_access_key_id = 'AKIAS252WFDQQHJJLDW5'
-    aws_secret_access_key = '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'
-    region_name = 'us-east-1'
-    bucket_name = 'klaviyobucket'
+    aws_access_key_id = os.getenv('aws_access_key_id')
+    aws_secret_access_key = os.getenv('aws_secret_access_key')
+    region_name = os.getenv('region_name')
+    bucket_name = os.getenv('bucket_name')
 
     # Create an S3 client with hardcoded credentials
     s3 = boto3.client(
